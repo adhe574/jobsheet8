3765a482b3f250d50680f057a1403f1b2a4f0122
adhe574 <adhewidya378@gmail.com>
2024-10-25 13:56:14 +0700

    simpan

---
+++ Star01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Star01 {
+    public static void main(String[] args) {
+        
+        Scanner sc01 = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = sc01.nextInt();
+        
+        for (int i=1; i<=N; i++) {
+            System.out.print("*");
+        }
+    }
+}
