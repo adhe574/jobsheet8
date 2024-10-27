f99bcab7fe47420855fe5395bab20012a7e5686c
adhe574 <adhewidya378@gmail.com>
2024-10-27 10:21:13 +0700

    simpan

--- Square01.java
+++ Square01.java
@@ -2,11 +2,16 @@ import java.util.Scanner;
 public class Square01 {
     public static void main(String[] args) {
         
-        Scanner sc01 = new Scanner(System.in);        
+        Scanner sc01 = new Scanner(System.in);
+        
         System.out.print("Masukkan nilai N = ");
         int N = sc01.nextInt();
-        for(int i=1; i<=N; i++) {
-            System.out.print("*");
+        
+        for (int iOuter=1; iOuter<=N; iOuter++) {
+        for (int i=0; i<=N; i++) {
+                    System.out.print("*");
+        }
+        
         }
     }
 }
