a77a0bc143c6f4b56efd7ef2fb0463af37217c96
adhe574 <adhewidya378@gmail.com>
2024-10-31 18:06:51 +0700

    simpan

--- RataNilai01.java
+++ RataNilai01.java
@@ -3,32 +3,24 @@ import java.util.Scanner;
 public class RataNilai01 {
    public static void main(String[] args) {       
     Scanner sc01 = new Scanner(System.in);
-    int i, j;
-    float nilai, totalNilai, rataNilai;
-
-    for (i=1;j<=5;i++) {
-        totalNilai=0;
-        System.out.println("Input Nilai Mahasiswa ke " + i);
-
-        for (i=1;j<=5;j++);
-            System.out.print("Nilai ke-" +j + " = ");
-            nilai = sc01.nextFloat();
-            totalNilai+=nilai;
-           
-    }
+    int i, j, nilaiMahasiswa;
+    float totalNilai, rataNilai;
 
     i=1;
     while (i<=5) {
         totalNilai=0;
-        for (j=1;j<=5;j++) {
-            System.out.print("Nilai ke-" +j + " = ");
-            nilai=sc01.nextInt();
-            totalNilai+=nilai;
-        }
+        System.out.println("Input Nilai Mahasiswa ke " + i);
+ 
+    for (j=1;j<=5;j++) {
+        System.out.print("Nilai ke " + j + " = ") ;
+        nilaiMahasiswa=sc01.nextInt();
+        totalNilai += nilaiMahasiswa;
+           
+    }
         rataNilai=totalNilai/5;
         System.out.println("Rata-rata Nilai Mahasiswa ke " + i +" adalah " + rataNilai);
+        System.out.println();
         i++;
     }
-
    } 
 }
