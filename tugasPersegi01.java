bf25970209d8802558d753f71b9bf7c309a11deb
adhe574 <adhewidya378@gmail.com>
2024-10-31 18:33:55 +0700

    simpan

--- tugasPersegi01.java
+++ tugasPersegi01.java
@@ -4,23 +4,27 @@ public class tugasPersegi01 {
     public static void main(String[] args) {        
         Scanner sc01 = new Scanner(System.in);
 
-        System.out.print("Masukkan nilai N = ");
-        int N = sc01.nextInt();
-        int i = 0;
+        int N, i, j;
 
-        if (N < 3) {
-            System.out.println("Masukkan nilai N (minimal 3): ");
-        } else {
-            for (i=0; i<N; i++) {
-                for (int j=0; j<N; j++) {
-                    if (i==0 || i==N-1 || j==0 ||j==N-1) {
-                        System.out.print("5");
+        do { 
+            System.out.print("Masukkan nilai N (minimal 3): ");
+            N = sc01.nextInt();
+        
+            if (N < 3) {
+                System.out.println("nilai N harus minimal 3. Masukkan nilai N yang benar!");
+            }
+
+            } while (N < 3);
+
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < N; j++) {
+                    if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
+                        System.out.print(N + " ");
                     } else {
-                        System.out.print("3");
+                        System.out.print("  ");
                     }
                 }
                 System.out.println();
             }
-        }
     }
 }
