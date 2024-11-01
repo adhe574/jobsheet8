53ee9eec02150d8ac42b68b59018f7f856cbc3c7
adhe574 <adhewidya378@gmail.com>
2024-11-01 18:48:16 +0700

    simpan

--- Porseni01.java
+++ Porseni01.java
@@ -7,7 +7,7 @@ public class Porseni01 {
         int jumlahPoliteknik, i, k;
         String namaPoliteknik, dataAtlet, namaAtlet;
 
-        System.out.print("Masukkan jumlah Politeknik yang mendafar: ");
+        System.out.print("Masukkan Jumlah Politeknik yang Mendafar: ");
         jumlahPoliteknik = sc01.nextInt();
         sc01.nextLine();
 
@@ -17,21 +17,22 @@ public class Porseni01 {
         for (i=1; i<=jumlahPoliteknik; i++) {
              System.out.print("Masukkan Nama Politeknik ke- " + i + " : " );
              namaPoliteknik = sc01.nextLine();
-             dataAtlet += " Politeknik " + i + " (" + namaPoliteknik + "):\n";
+             dataAtlet += "Politeknik" + i + " (" + namaPoliteknik + "):\n";
 
              for (String cabang : cabangOlahraga) {
-                dataAtlet += " Cabang " + cabang + ":\n";
+                dataAtlet += "Cabang" + cabang + ":\n";
 
                 for (k=1; k<=5; k++) {
                     System.out.print("Masukkan nama atlet ke- " + k + " untuk cabang " + cabang + " : ");
                     namaAtlet = sc01.nextLine();
-                    dataAtlet += " Atlet " + k + " : " + namaAtlet + "\n";
+                    dataAtlet += "Atlet" + k + " : " + namaAtlet + "\n";
                 }
              }
             dataAtlet += "\n";
-        }  
-        System.out.println(" Nama Atlet Porseni 2024 ");
+
+        System.out.println("Nama Atlet Porseni 2024");
         System.out.println(dataAtlet);
+        }  
         sc01.close();
     }
 }
